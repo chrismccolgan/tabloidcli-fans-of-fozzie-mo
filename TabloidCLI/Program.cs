2a80eccf591d831229a42bf2f3fc800a5f325e4f
chrismccolgan <mccolganchrisj@gmail.com>
2020-09-11 13:53:04 -0500

    code cleanup

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -7,17 +7,22 @@ namespace TabloidCLI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(@"HI THERE!!! WAKA WAKA");
             Console.WriteLine(@"
-  _(___)_  
- ()'   `() 
- .' o o `. 
- :  _O_  : --- Welcome to Tabloid.ly by Team Fans of Fozzie.
- `. \_/ .' --- I'm Fozzie.
-  .`---'.     --- I hope your experience is a pleasant one. Waka waka!
-.' ()o() `.
-:   ( \   : 
-            ");
-            
+                                  _(___)_  
+                                 ()'   `() 
+                                 .' o o `. 
+                                 :  _O_  : 
+                                 `. \_/ .' --- I'm Fozzie!
+                                  .`---'.     --- I hope your experience is a pleasant one!
+                                .' ()o() `.
+                                :   ( \   : ");
+
+            Console.WriteLine("Welcome to Tabloid.ly by Team Fans of Fozzie. Waka waka!");
+            string strDate = "2020-01-01";
+            DateTime theDate = DateTime.Parse(strDate);
+            Console.WriteLine(theDate);
+
             // MainMenuManager implements the IUserInterfaceManager interface
             IUserInterfaceManager ui = new MainMenuManager();
             while (ui != null)
